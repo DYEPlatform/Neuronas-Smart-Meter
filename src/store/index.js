036eedc2f608d6cc7afbd722d1fe085f2b3a35b8
krouw <felipe.andrez@live.cl>
2018-01-02 07:45:31 -0300

    v0.6.0

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,30 @@
-const redux = require('redux')
-const createLogger = require('redux-node-logger')
-const rootReducer = require('../reducer')
+const redux = require('redux'),
+      createLogger = require('redux-node-logger'),
+      thunk = require('redux-thunk').default,
+      rootReducer = require('../reducer');
 
 // Middlewares
 const loggerMiddleware = createLogger();
 
-module.exports = redux.createStore(
-  rootReducer,
-  undefined,
-  redux.compose(
-    redux.applyMiddleware( loggerMiddleware ),
-  ),
-);
+let store = {}
+
+if ( process.env.NODE_ENV === 'test') {
+  store = redux.createStore(
+    rootReducer,
+    undefined,
+    redux.compose(
+      redux.applyMiddleware( thunk ),
+    )
+  );
+}
+else {
+  store = redux.createStore(
+    rootReducer,
+    undefined,
+    redux.compose(
+      redux.applyMiddleware( loggerMiddleware, thunk ),
+    ),
+  );
+}
+
+module.exports = store
